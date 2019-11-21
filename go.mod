b3b429f56a40cdb8ffb0b0bef41bffc312dad45f
Travis Tomsu <ttomsu@google.com>
2019-11-21 12:04:26 -0500

    fix(proto): Use Kork's proto as the source of truth (#8)

--- go.mod
+++ go.mod
@@ -6,9 +6,10 @@ require (
 	cloud.google.com/go v0.43.0
 	cloud.google.com/go/logging v1.0.0
 	github.com/golang/protobuf v1.3.2
-	github.com/grpc-ecosystem/grpc-gateway v1.11.1 // indirect
+	github.com/grpc-ecosystem/grpc-gateway v1.12.1 // indirect
 	github.com/mitchellh/protoc-gen-go-json v0.0.0-20190813154521-ece073100ced // indirect
 	github.com/oklog/ulid/v2 v2.0.2
-	google.golang.org/genproto v0.0.0-20190911173649-1774047e7e51 // indirect
-	gopkg.in/yaml.v2 v2.2.2 // indirect
+	github.com/spinnaker/kork v6.18.1+incompatible // indirect
+	google.golang.org/genproto v0.0.0-20191115221424-83cc0476cb11 // indirect
+	gopkg.in/yaml.v2 v2.2.7 // indirect
 )
--- proto/regen-protos.sh
+++ proto/regen-protos.sh
@@ -10,11 +10,17 @@ GOBIN=${GOBIN:=$GOPATH/bin}
 
 go get -u github.com/golang/protobuf/protoc-gen-go
 go get -u github.com/mitchellh/protoc-gen-go-json
+go get -u github.com/spinnaker/kork
 
 if [[ ":$PATH:" != *":${GOBIN}:"* ]]; then
 	echo "Adding $GOBIN to PATH"
 	export PATH=$PATH:$GOBIN
 fi
 
-./protoc --go_out=paths=source_relative:. --go-json_out=. **/*.proto
+KORK_VERSION=`grep -oP "(?<=kork ).*(?= //)" $SCRIPT_DIR/../go.mod`
+
+./protoc --go_out=paths=source_relative:$SCRIPT_DIR/stats/ \
+  --go-json_out=$SCRIPT_DIR/stats/ \
+  --proto_path=$GOPATH/pkg/mod/github.com/spinnaker/kork@$KORK_VERSION/kork-proto/src/main/proto/stats/ \
+  $GOPATH/pkg/mod/github.com/spinnaker/kork@$KORK_VERSION/kork-proto/src/main/proto/stats/*.proto
 popd
